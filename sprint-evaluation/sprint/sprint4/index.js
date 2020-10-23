f9d299943317f1747336b63a9eef2cfde2f6e258
yusuf <ysfozturk3@gmail.com>
2020-10-23 10:29:58 +0200

    created and completed html and css files.

---
+++ sprint-evaluation/sprint/sprint4/index.js
@@ -0,0 +1,17 @@
+
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+const result = document.querySelector('#result');
+
+button.addEventListener("click", Finder)
+
+    function Finder() {
+    let number = input.value;
+    console.log(number)
+
+
+ 
+    
+    
+}
+
