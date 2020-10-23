cdae9807f83087170e919108d3c129c2f3f26d0f
yusuf <ysfozturk3@gmail.com>
2020-10-23 12:24:38 +0200

    codes for find to perfect numbers-completed

--- sprint-evaluation/sprint/sprint4/index.js
+++ sprint-evaluation/sprint/sprint4/index.js
@@ -3,15 +3,32 @@ const input = document.querySelector("#input");
 const button = document.querySelector("#button");
 const result = document.querySelector('#result');
 
+
 button.addEventListener("click", Finder)
 
     function Finder() {
     let number = input.value;
-    console.log(number)
+    let elements = [...range(1,number)]
+    let divisors = elements.filter(e => number % e == 0)
+    total(divisors) == number ? result.innerHTML = 'This is a perfect Number' : result.innerHTML = 'This is not a perfect number'
+    input.value = ''
+    input.focus()    
+    }
 
 
- 
-    
-    
+function range(start,end) {
+    rangedNumbers = []
+    for(let i = start; i < end; i++){
+       rangedNumbers.push(i)
+    }
+    return rangedNumbers
+}
+
+function total(list) {
+    let addition = 0
+    list.forEach(element => {
+     addition += element   
+    });
+    return addition
 }
 
